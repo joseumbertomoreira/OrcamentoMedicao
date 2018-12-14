5dbf705569aa7d1673176dae0e2823c5223b200c
jose <jose@localhost.localdomain>
2018-12-14 14:17:27 -0200

    dab is already

--- server/app.js
+++ server/app.js
@@ -10,20 +10,18 @@ app.use(express.static(__dirname + "./../client"));
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }))
 
-
-
-
-/*()
 const url = 'mongodb://address:i123456@ds163020.mlab.com:63020/address-search';
 
 const client = new MongoClient(url);
-
 const dbName = 'address-search';
+
+/*()
+
 MongoClient.connect(url, function(err, client) {
 	let db = client.db('address')
 	let songs = db.collection('songs');
 
-	songs.insertOne(seedData, function(err, result) {
+	ad.insertOne(seedData, function(err, result) {
 
 	});
 
@@ -44,17 +42,25 @@ let distrctSearch = function(district, districtsJson){
 app.post('/form', (req, resp) =>{
 
 	let streetSplit = req.body.street.split(" ");
+	MongoClient.connect(url, function(err, client) {
+		let db = client.db('address')
+		let ad = db.collection('addressData');
 
-	request(`https://viacep.com.br/ws/GO/Goiania/${streetSplit[0]}+${streetSplit[1]}/json/`,
-	 { json: true }, (err, res, districtsJson) => {
-	  	if (err) { return console.log(err); }
-	  	if(districtsJson  !== [] && distrctSearch(req.body.district, districtsJson) !== -1){
-	  		resp.send(distrctSearch(req.body.district, districtsJson));
-	  	}else{
-	  		resp.send();
-	  	}
-	  	
-	});
+		request(`https://viacep.com.br/ws/GO/Goiania/${streetSplit[0]}+${streetSplit[1]}/json/`,
+		 { json: true }, (err, res, districtsJson) => {
+		  	if (err) { return console.log(err); }
+		  	if(districtsJson  !== [] && distrctSearch(req.body.district, districtsJson) !== -1){
+		  		ad.insertOne(districtsJson, function(err, result) {
+		  			client.close();
+		  			resp.send(distrctSearch(req.body.district, districtsJson));
+					});
+		  	}else{
+		  		client.close();
+		  		resp.send();
+		  	}
+		  })
+		  	
+		});
 
 })
 
